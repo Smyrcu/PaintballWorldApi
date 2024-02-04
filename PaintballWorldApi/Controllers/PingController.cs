e80a2a9518fb02e1280cbf3b434c46187547978e
Błażej Szadkowski <42031920+Smyrcu@users.noreply.github.com>
2024-02-04 21:43:52 +0100

    Ping

---
+++ PaintballWorldApi/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PaintballWorld.API.Controllers
+{
+    [ApiController]
+    [AllowAnonymous]
+    [Route("/api/[controller]")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        [Route("Ping")]
+        public Task<IActionResult> Ping()
+        {
+            return Task.FromResult<IActionResult>(Ok("I'm alive!"));
+        }
+    }
+}
