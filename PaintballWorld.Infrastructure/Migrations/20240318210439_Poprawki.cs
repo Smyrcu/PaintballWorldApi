9fe37cc07631f84b94f855021147f2eeedaf32ec
Błażej Szadkowski <42031920+Smyrcu@users.noreply.github.com>
2024-03-18 22:05:50 +0100

    changes

---
+++ PaintballWorld.Infrastructure/Migrations/20240318210439_Poprawki.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PaintballWorld.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Poprawki : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "FieldSchedule");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "FieldSchedule",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Date",
+                table: "FieldSchedule",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "Time",
+                table: "FieldSchedule",
+                type: "time",
+                nullable: true);
+        }
+    }
+}
