52f81e6d39d638046e20d7bc6e93a1058fb5f9b3
Błażej Szadkowski <42031920+Smyrcu@users.noreply.github.com>
2024-05-05 17:12:03 +0200

    history

---
+++ PaintballWorldApi/Areas/User/Controllers/UserHistoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PaintballWorld.API.Areas.User.Models;
+using PaintballWorld.Infrastructure;
+using PaintballWorld.Infrastructure.Interfaces;
+
+namespace PaintballWorld.API.Areas.User.Controllers
+{
+    [Route("api/[area]/[controller]")]
+    [Area("User")]
+    [ApiController]
+    public class UserHistoryController(ApplicationDbContext context, IAuthTokenService authTokenService) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyHistory()
+        {
+            var userId = authTokenService.GetUserId(User.Claims);
+
+            var history = context.UsersToEvents
+                .Include(x => x.Event)
+                .ThenInclude(x => x.Field)
+                .ThenInclude(y => y.Sets)
+                .Where(x => x.UserId == userId && x.Event.EndDate < DateTime.UtcNow)
+                .Select(x => new Game
+                {
+                    Field = new Models.Field
+                    {
+                        FieldId = x.Event.Field.Id.Value,
+                        FieldName = x.Event.Field.Name
+                    },
+                    Event = new Models.Event
+                    {
+                        EventId = x.Event.Id.Value,
+                        Ammo = x.set.Ammo,
+                        Price = x.set.Price,
+                        Date = x.Event.StartDate
+                    }
+                })
+                .ToList();
+
+            var historyModel = new HistoryModel
+            {
+                GamesPlayed = history.Count,
+                games = history
+            };
+
+            return Ok(historyModel);
+
+        }
+    }
+}
---
+++ PaintballWorldApi/Areas/User/Models/HistoryModel.cs
@@ -0,0 +1,29 @@
+﻿namespace PaintballWorld.API.Areas.User.Models
+{
+    public class HistoryModel
+    {
+        public int GamesPlayed { get; set; }
+        public List<Game> games { get; set; } = [];
+
+    }
+
+    public class Game
+    {
+        public Field Field { get; set; }
+        public Event Event { get; set; }
+    }
+
+    public class Field
+    {
+        public Guid FieldId { get; set; }
+        public string? FieldName { get; set; }
+    }
+
+    public class Event
+    {
+        public Guid EventId { get; set; }
+        public int? Ammo { get; set; }
+        public decimal? Price { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
