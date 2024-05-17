73fa7351124cb6357b2bcaff9898bc7fd1dca92d
Błażej Szadkowski <42031920+Smyrcu@users.noreply.github.com>
2024-05-17 22:16:23 +0200

    diagram klas

--- PaintballWorld.Infrastructure/ApplicationDbContext.cs
+++ PaintballWorld.Infrastructure/ApplicationDbContext.cs
@@ -64,7 +64,7 @@ public partial class ApplicationDbContext : IdentityDbContext
 
     public virtual DbSet<Contact> Contacts { get; set; }
 
-#if !DEBUG
+#if DEBUG
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=PaintballWorldApp2;Integrated Security=true;",
             provider =>
@@ -74,8 +74,8 @@ public partial class ApplicationDbContext : IdentityDbContext
 #else
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
      => optionsBuilder.UseSqlServer(
-         "Server=127.0.0.1,9210;User Id=sa;Password=JakiesLosoweHaslo123;Database=PaintballWorldApp2;Trusted_Connection=False;MultipleActiveResultSets=true;Encrypt=false",
-         //          "Server=192.168.1.191,1433;User Id=sa;Password=JakiesLosoweHaslo123;Database=PaintballWorldApp2;Trusted_Connection=False;MultipleActiveResultSets=true;Encrypt=false",
+         //"Server=127.0.0.1,9210;User Id=sa;Password=JakiesLosoweHaslo123;Database=PaintballWorldApp2;Trusted_Connection=False;MultipleActiveResultSets=true;Encrypt=false",
+                "Server=192.168.1.191,1433;User Id=sa;Password=JakiesLosoweHaslo123;Database=PaintballWorldApp2;Trusted_Connection=False;MultipleActiveResultSets=true;Encrypt=false",
          providerOptions =>
          {
              providerOptions.EnableRetryOnFailure(
